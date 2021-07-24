7564f9eb55316fcd6c9d17aa2112838af148dba5
CrazyRedMachine <36906596+CrazyRedMachine@users.noreply.github.com>
2021-07-25 01:33:49 +0200

    Update usb_rename.h

--- usb_rename.h
+++ usb_rename.h
@@ -5,7 +5,9 @@ class USBRename : public PluggableUSBModule {
     USBRename(
       const u8 *product_name = NULL,
       const u8 *manufacturer_name = NULL,
-      const u8 *serial_num = NULL);
+      const u8 *serial_num = NULL,
+      const u16 vid = 0x2341,
+      const u16 pid = 0x8036);
     int getInterface(uint8_t* interfaceCount);
     int getDescriptor(USBSetup& setup);
     bool setup(USBSetup& setup);
@@ -14,5 +16,7 @@ class USBRename : public PluggableUSBModule {
     const u8 *manufacturer_name;
     const u8 *product_name;
     const u8 *serial_num;
+    const u16 vid;
+    const u16 pid;
     uint8_t epType[0];
 };
